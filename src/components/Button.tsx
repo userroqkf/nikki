15a07436eadbbdb11aeaf54adbc0f60fafba1f1e
John Kim <96427720+userkimjh@users.noreply.github.com>
2023-08-11 00:29:34 +0800

    changed button component to not resize by text input

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,13 @@ import styles from '../styles/Button.module.css';
 type Props =  {
   label: string;
   size? : 'small' | 'medium' | 'large';
+  style: 'solid' | 'empty';
 }
 
-export default function Button({ label, size } : Props) {
-  console.log(size, "size print", styles)
-  const className: string = `${styles.button} ${size === undefined ? "" : styles[size]}`  
+export default function Button({ label, size, style } : Props) {
+  const className: string = `${styles.button} ${size === undefined ? "" : styles[size]} ${styles[style]}`  
   return (
-    <button 
-      className={className}
-    > 
+    <button className={className}> 
       {label} 
     </button> 
   );
