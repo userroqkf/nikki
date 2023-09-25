4b85c597808d7145b3e26c3d92022f7e4a8047af
John Kim <96427720+userkimjh@users.noreply.github.com>
2023-09-25 12:53:36 +0800

    added loading spinner

---
+++ app/_components/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+import styles from "@/_styles/LoadingSpinner.module.css"
+export default function LoadingSpinner() {
+  return (
+    <div className={styles.box}>
+      <span className={styles.loader}></span>
+    </div>
+  )
+}
