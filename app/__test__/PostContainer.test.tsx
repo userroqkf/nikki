fa361cf5011aea46c5925f4e198449578d91f2ea
John Kim <96427720+userkimjh@users.noreply.github.com>
2023-09-25 11:45:03 +0800

    added test cases for components

---
+++ app/__test__/PostContainer.test.tsx
@@ -0,0 +1,42 @@
+import PostContainer from '@/_components/PostContainer'
+import '@testing-library/jest-dom'
+import {render, screen, fireEvent} from '@testing-library/react'
+
+describe('test PostContainer', () => {
+  const mockProps = {
+    profilePictureURL:"https://picsum.photos/id/237/200/300",
+    fullName:"Tom Tom",
+    username: "tomtom19238",
+    content: {
+      text: "This is a comment testing content string",
+      datePosted: "30s",
+      commentCount: 100,
+      likeCount: 11
+    },
+    liked: false
+  }
+
+  it('should render PostContainer', () => {
+    render(<PostContainer {...mockProps} />)
+  })
+
+  it('should have the corresponding text', () => {
+    render(<PostContainer {...mockProps} />)
+    const post = document.querySelector('.postContainer')
+    expect(post).toHaveTextContent("This is a comment testing content string");
+  })
+
+  it('check like button exists', () => {
+    render(<PostContainer {...mockProps} />)
+    const likeButton = screen.getAllByRole('button')[1]
+    expect(likeButton).toBeTruthy()
+  })
+
+  it('should change to editing mode when clicked', () => {
+    render(<PostContainer {...mockProps}/>)
+    const editingButton = screen.getByTitle("editButton")
+    fireEvent.click(editingButton)
+    expect(editingButton).not.toBeInTheDocument()
+  })
+
+})
--- app/_components/LikeButton.tsx
+++ app/_components/LikeButton.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
-import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import { faH, faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useMemo, useState } from "react";
 import styles from "@/_styles/LikeButton.module.css";
 type Props = {
@@ -8,22 +8,22 @@ type Props = {
   liked: boolean;
 }
 
-export default function LiekButton({likeCount, liked}: Props) {
+export default function LikeButton({likeCount, liked}: Props) {
   //Might be a better idea to use useMemo instead of useState
   const [like, setLike] = useState<boolean>(liked);
   const [likeCountUpdate, setLikeCountUpdage] = useState<number>(likeCount)
 
   const updateLikeButton = (e: React.SyntheticEvent) => {
     e.preventDefault()
-    !like ? setLikeCountUpdage(prev => prev - 1) : setLikeCountUpdage(prev => prev + 1)
+    like ? setLikeCountUpdage(prev => prev - 1) : setLikeCountUpdage(prev => prev + 1)
     setLike(!like);
   }
   return (
     <div>
       {
-       <div className={styles.likeButton}>
-          <FontAwesomeIcon onClick={updateLikeButton} icon={like ? faHeart : faHeartSolid} />
-       </div>
+        <button className={styles.likeButton} onClick={updateLikeButton}>
+          <FontAwesomeIcon  icon={like ? faHeartSolid : faHeart} />
+        </button>
       }
       <span>{likeCountUpdate}</span>
     </div> 
